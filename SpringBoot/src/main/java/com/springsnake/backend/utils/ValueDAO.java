2e31d6ab09ca01227f8fb4295182e6eb5682c703
Stefano Sciacovelli <code@morpheos.it>
2023-12-28 12:01:53 +0100

    Refactoring and Repurposing of the DAO
    In SpringBoot

--- SpringBoot/src/main/java/com/springsnake/backend/utils/ValueDAO.java
+++ SpringBoot/src/main/java/com/springsnake/backend/utils/ValueDAO.java
@@ -1,47 +1,30 @@
 package com.springsnake.backend.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 import com.springsnake.backend.values;
 
-import lombok.Data;
-
-@Data
-public class ValueDAO implements Dao<values> {
+public class ValueDAO{
 
     private ValueRepository valueRepo;
      
-    private List<values> inmemorydb = valueRepo.findAll();
-     
-    @Override
-    public Optional<values> get(long id) {
-        return Optional.ofNullable(inmemorydb.get((int) id));
+    private List<values> valuesList = new ArrayList<values>();
+
+    public List<values> getAll() {
+        return valuesList;
     }
 
-    @Override
-    public List<values> getAll() {
-        return inmemorydb;
+    public void saveAll(List<ValueDTO> input){
+        for (ValueDTO value : input) {
+            valuesList.add(new values(value.getKey(), value.getValue()));
+        }    
     }
-        
-    @Override
-    public void save(values value) {
-        inmemorydb.add(value);
+
+    public void close() {
+        valueRepo.saveAll(valuesList);
     }
-       
-    @Override
-    public void update(values value, String[] params) {
-        value.setKey(Objects.requireNonNull(
-            params[0], "Key cannot be null"));
-        value.setValue(Objects.requireNonNull(
-            params[1], "Value cannot be null"));
-         
-        inmemorydb.add(value);
-    }
-        
-    @Override
-    public void delete(values value) {
-        inmemorydb.remove(value);
-    }
+
 }
