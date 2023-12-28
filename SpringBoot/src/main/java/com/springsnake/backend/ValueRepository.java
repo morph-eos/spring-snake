dffd3530d5df5be983c13991665ecb27212cc2d4
Stefano Sciacovelli <code@morpheos.it>
2023-12-28 11:21:24 +0100

    Repository Logic Moved, DAO Implemented
    Repository Logic now into Service.
    DAO is now implemented

--- SpringBoot/src/main/java/com/springsnake/backend/ValueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springsnake.backend;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ValueRepository extends MongoRepository<values, String> {
-    values findByKey(String key);
-}
--- SpringBoot/src/main/java/com/springsnake/backend/ValueService.java
+++ SpringBoot/src/main/java/com/springsnake/backend/ValueService.java
@@ -1,16 +1,46 @@
 package com.springsnake.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import com.springsnake.backend.Dao;
 
 @AllArgsConstructor
 @Service
 public class ValueService {
     
+    @Repository
+    private interface ValueRepository extends MongoRepository<values, String> {
+        values findByKey(String key);
+    }
+
     @Autowired
     private final ValueRepository valueRepo;
 
+    @Data
+    public class ValueDAO implements Dao<values> {
+        
+        private List<values> inmemorydb = valueRepo.findAll();
+        
+        @Override
+        public Optional<values> get(long id) {
+            return Optional.ofNullable(inmemorydb.get((int) id));
+        }
+    }
+    private static class ValueDAO {
+
+
+        private String key;
+        private Object value;
+    }
+    
     public Object get(String key) {
         values value = valueRepo.findByKey(key);
          if (value == null) {
